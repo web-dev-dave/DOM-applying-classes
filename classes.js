05ed6128d59b658baae4a6d10f3d3b9a68d6b707
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-09-11 13:39:08 +1200

    create functions

--- classes.js
+++ classes.js
@@ -1,25 +1,46 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one();
+  two();
+  three();
+  makeVisible();
+  console.log(document.getElementsByClassName('invisible')[0]);
+
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two');
 
+  two.classList.add('green');
+}
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three');
+
+  three.classList.add('orange');
+
+}
+
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var four = document.getElementsByClassName('invisible')[0];
+
+  four.classList.add('visible');
+}
